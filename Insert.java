5f58f3e27b4dfabd7170810627817baf8c94edd3
mincatsg <1374723981@qq.com>
2019-12-04 21:43:37 +0800

    插入数据到数据库

---
+++ Insert.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class Insert {
+    public static void main(String[] args) {
+       insert(8,"阿三", 70);
+    }
+    public static void insert(int id, String name, int grace) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            try{
+                //创建数据库连接Connection
+                connection = DEUTil.getConnection2();
+                //根据数据库连接创建操作命令对象Statement
+                String sql = "insert into exam_result(id,name,math) values" + " (?,?,?)";
+                statement = connection.prepareStatement(sql);
+                //操作命令对象执行sql语句,返回结果集resultSet
+                //resultSet类似List<Map<String, Object>>
+                statement.setInt(1,id);
+                statement.setString(2,name);
+                statement.setInt(3,grace);
+                //新增 修改 删除 操作 ,都使用exexuteUpdate()方法
+                int num = statement.executeUpdate();
+                if(num > 0){
+                    System.out.println("修改成功");
+                }else{
+                    System.out.println("修改失败");
+                }
+            }finally {
+                //释放资源
+                DEUTil.close(connection, statement, null);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
