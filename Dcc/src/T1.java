5c86f212b61ed8b2ce70eae303128a1e249af295
mincatsg <1374723981@qq.com>
2019-12-10 00:01:00 +0800

    1

---
+++ Dcc/src/T1.java
@@ -0,0 +1,25 @@
+public class T1 {
+public static void main(String[] args) {
+int k = 10 ;
+int[] num = new int[k];
+for(int i = 0;i<num.length;i++){
+num[i] = rodem();
+}
+print(num);
+}
+public static int rodem(){
+int i = (int)(Math.random()*100) ;
+if(i>50){
+return i ;
+}
+return rodem();
+}
+public static void print(int[] n){
+for(int i = 0;i<n.length;i++){
+if(i != 0){
+System.out.print(",") ;
+}
+System.out.print(n[i]) ;
+}
+}
+}
---
+++ Dcc/src/mianji.java
@@ -0,0 +1,12 @@
+
+   interface tt{
+	   double r=3.14;
+	   public void yuan();
+	   public void juxing();
+   }
+   public class wu implements tt {
+	   int x;
+	   
+   }
+
+
